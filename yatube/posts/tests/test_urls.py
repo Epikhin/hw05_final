5628de3831c80dc505d42afa8d2d1dda4236d37c
Epikhin <114134984+Epikhin@users.noreply.github.com>
2022-11-29 23:42:38 +0700

    asd

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,23 +1,31 @@
-from http import HTTPStatus
 
-from ..models import Group, Post, User
-from django.test import TestCase, Client
+from django import forms
+from django.test import Client, TestCase
+from django.urls import reverse
 from django.core.cache import cache
 
+from ..views import CONST_POST
 
-class StaticURLTests(TestCase):
+from ..models import Group, Post, User
+
+CONST_POST1 = 3
+CONST_POST2 = 13
+
+
+class PostViewsTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create_user(username='qwerty')
-        cls.post = Post.objects.create(
-            author=cls.user,
-            text='Текстовый пост',
-        )
         cls.group = Group.objects.create(
             title='Тестовая группа',
             slug='slug_slug',
             description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+            group=cls.group
         )
 
     def setUp(self):
@@ -26,83 +34,136 @@ class StaticURLTests(TestCase):
         self.authorized_client.force_login(self.user)
         cache.clear()
 
-    def test_task_list_url_uses_correct_template(self):
-        response = self.authorized_client.get('/create/')
-        self.assertTemplateUsed(response, 'posts/post_create.html')
-
-    def test_url_author(self):
-        response = self.authorized_client.get('/posts/1/edit/')
-        self.assertTemplateUsed(response, 'posts/post_create.html')
-
-    def test_private_url(self):
-        """Без авторизации приватные URL недоступны"""
-        url_names = (
-            'create/',
-            'posts/<int:post_id>/edit/',
-        )
-        for adress in url_names:
-            with self.subTest():
-                response = self.guest_client.get(adress)
-                self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-
-    def test_url_status_code(self):
-        url_status = {
-            '/': 200,
-            '/group/slug_slug/': 200,
-            '/profile/qwerty/': 200,
-            '/posts/1/': 200,
-            '/unexisting_page/': 404,
+    def test_pages_uses_correct_template(self):
+        templates_pages_names = {
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:profile', kwargs={'username': 'qwerty'}
+                    ): 'posts/profile.html',
+            reverse('posts:post_detail', kwargs={'post_id': '1'}
+                    ): 'posts/post_detail.html',
+            reverse('posts:post_create'): 'posts/post_create.html',
+            reverse('posts:group_list', kwargs={'slug': 'slug_slug'}
+                    ): 'posts/group_list.html',
+            reverse('posts:post_edit', kwargs={'post_id': '1'}
+                    ): 'posts/post_create.html'
         }
-        for template, url in url_status.items():
-            with self.subTest(url=url):
-                response = self.guest_client.get(template)
-                self.assertEqual(response.status_code, url)
 
-    def test_new_for_authorized(self):
-        """Страница /create доступна авторизованному пользователю."""
-        response = self.authorized_client.get('/create/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        for reverse_name, template in templates_pages_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
 
-    def test_post_create_url_redirect_anonymous(self):
-        """Страница /create перенаправляет анонима."""
-        response = self.guest_client.get('/create/', follow=True)
-        self.assertRedirects(
-            response, ('/auth/login/?next=/create/'))
+    def test_index_page_show_correct_context(self):
+        """Шаблон index сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('posts:index'))
+        first_object = response.context['page_obj'][0]
+        self._assert_post(first_object)
 
-    def test_follow_url(self):
-        """Страница /follow доступна авторизованному пользователю."""
-        response = self.authorized_client.get('/follow/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def _assert_post(self, first_object):
+        self.assertEqual(first_object.group.title, self.group.title)
+        self.assertEqual(first_object.text, self.post.text)
+        self.assertEqual(first_object.group.slug, self.group.slug)
 
-    def test_post_edit_url_redirect_anonymous(self):
-        """Страница posts/<int:post_id>/edit/ перенаправляет анонима."""
-        response = self.guest_client.get('/posts/1/edit/', follow=True)
-        self.assertRedirects(
-            response, ('/auth/login/?next=/posts/1/edit/'))
+    def test_post_another_group(self):
+        """Пост не попал в другую группу"""
+        response = self.authorized_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'slug_slug'}))
+        first_object = response.context["page_obj"][0]
+        post_text_0 = first_object.text
+        self.assertTrue(post_text_0, 'Тестовая запись для создания 2 поста')
 
-    def test_create_url_redirect_anonymous(self):
-        """Страница /create перенаправляет не авторизованного пользователя."""
-        response = self.guest_client.get('/create/', follow=True)
-        self.assertRedirects(response, '/auth/login/?next=/create/')
+    def test_group_list_page_show_correct_context(self):
+        """Шаблон group_list сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse(
+            'posts:group_list', kwargs={'slug': 'slug_slug'}))
+        first_object = response.context['page_obj'][0]
+        self._assert_post(first_object)
+        self.assertEqual(len(response.context['page_obj']), 1)
 
-    def test_posts_post_edit_url(self):
-        """Страница posts/post_id/edit/ доступна только автору."""
+    def test_profile_page_show_correct_context(self):
+        """Шаблон profile сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse(
+            'posts:profile', kwargs={'username': 'qwerty'}))
+        first_object = response.context['page_obj'][0]
+        post_group_0 = first_object.author
+        self._assert_post(first_object)
+        self.assertEqual(post_group_0, PostViewsTests.post.author)
+        self.assertEqual(len(response.context['page_obj']), 1)
+
+    def test_post_detail_page_show_correct_context(self):
+        """Шаблон post_detail сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse(
+            'posts:post_detail', kwargs={'post_id': self.post.pk}))
+        first_object = response.context['post']
+        self.assertEqual(first_object.pk, PostViewsTests.post.pk)
+
+    def test_create_post_page_show_correct_context(self):
+        """Шаблон post_create сформирован с правильным контекстом."""
+        response = self.authorized_client.get(reverse('posts:post_create'))
+        form_fields = {
+            'text': forms.fields.CharField,
+            'group': forms.models.ModelChoiceField,
+        }
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
+
+    def test_cache(self):
+        """Проверка работы кэша"""
+        response = self.authorized_client.get(reverse('posts:index'))
+        with_cache = response.content
+        first_post = response.context['page_obj'][0]
+        first_post.delete()
+        response = self.authorized_client.get(reverse('posts:index'))
+        after_delete_bd = response.content
+        cache.clear()
+        response = self.authorized_client.get(reverse('posts:index'))
+        after_clean_cache = response.content
+        self.assertEqual(with_cache, after_delete_bd, 'Ошибка')
+        self.assertNotEqual(after_delete_bd, after_clean_cache, 'Ошибка')
+
+
+class PaginatorViewsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='saint')
+        cls.group1 = Group.objects.create(
+            title='Тестовая группа',
+            slug='slug_slug',
+            description='Тестовое описание',
+        )
+        cls.posts = []
+        for i in range(CONST_POST2):
+            cls.posts.append(Post(
+                text=f'Тестовый пост {i}',
+                author=cls.user,
+                group=cls.group1
+            )
+            )
+        Post.objects.bulk_create(cls.posts)
+
+    def setUp(self):
+        self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-        response = self.authorized_client.get(f'/posts/{self.post.id}/edit/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        cache.clear()
 
-    def test_urls_uses_correct_template(self):
-        """URL адреса и шаблон"""
-        templates_url_names = {
-            '/': 'posts/index.html',
-            f'/group/{self.group.slug}/': 'posts/group_list.html',
-            f'/posts/{self.post.id}/': 'posts/post_detail.html',
-            '/create/': 'posts/post_create.html',
-            f'/profile/{self.user}/': 'posts/profile.html',
-            f'/posts/{self.post.id}/edit/': 'posts/post_create.html',
-        }
-        for address, template in templates_url_names.items():
-            with self.subTest(template=template):
-                response = self.authorized_client.get(address)
-                self.assertTemplateUsed(response, template)
+    def test_first_page_contains_ten_records(self):
+        response = self.client.get(reverse('posts:index'))
+        self.assertEqual(len(response.context['page_obj']), CONST_POST)
+
+    def test_second_page_contains_three_records(self):
+        response = self.client.get(reverse('posts:index') + '?page=2')
+        self.assertEqual(len(response.context['page_obj']), CONST_POST1)
+
+    def test_first_page_group_list_contains_ten_records(self):
+        response = self.client.get(reverse(
+            'posts:group_list', kwargs={'slug': 'slug_slug'}))
+        self.assertEqual(len(response.context['page_obj']), CONST_POST)
+
+    def test_first_page_profile_contains_ten_records(self):
+        response = self.authorized_client.get(reverse(
+            'posts:profile', kwargs={'username': 'saint'}))
+        self.assertEqual(len(response.context['page_obj']), CONST_POST)
